9feb82adb0f4209abcbe973696becf3c4d3c5723
mohdmuaz <91714800+mohdmuaz@users.noreply.github.com>
2024-10-21 16:43:44 +0000

    Comaprision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 == 1);
+// console.log(2 != 1); 
+
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+// JS convert convert it into num automaticaly
+
+
+// These type of conversion alwase make confussion
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+
+// === (Strict check in this it does not convert the data type)
+
+console.log("2" === 2);
